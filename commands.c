5c1501f00012f3726b4e87caedb51f2fc8ce95d8
ianwu13 <54074286+ianwu13@users.noreply.github.com>
2020-11-12 22:17:18 -0500

    Add files via upload
    These files contain the functions to be called by the main method in "shell" and will execute the internal commands that need to be implemented for this project

---
+++ commands.c
@@ -0,0 +1,17 @@
+#include "commands.h"
+
+//Part 1 Commands
+
+
+
+//Part 2 Commands
+
+void cd(char* path){
+
+}
+
+void path(char arg1, char* arg2){
+//arg1 should be '+', '-', or '\0'
+//arg2 should be the extension to be appended or subrtacted from the path. "\0" if arg1 is '\0'
+
+}
---
+++ commands.h
@@ -0,0 +1,14 @@
+#ifndef COMMANDSHEADER
+#define COMMANDSHEADER
+
+//Part 1 Commands
+
+
+
+//Part 2 Commands
+
+void cd(char* path);
+
+void path(char arg1, char* arg2);
+
+#endif
